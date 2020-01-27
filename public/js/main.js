c29310940ff61ea8e50c6c18ac02066b6ac5a279
Zakir Hossen <web.zakirbd@gmail.com>
2020-01-27 20:15:10 +0600

    main.js delete request updated

--- public/js/main.js
+++ public/js/main.js
@@ -5,13 +5,13 @@ $.ajaxSetup({
 });
 
 // Create Task
-$('#createTaskForm').submit(function(e){
+$('#createTaskForm').submit(function (e) {
     e.preventDefault();
 
     let msg = $('#createTaskMessage');
     // Form data
     let input = $('#createTaskForm input[name="name"]');
-    let formData  = {
+    let formData = {
         name: $(input).val()
     }
 
@@ -19,7 +19,7 @@ $('#createTaskForm').submit(function(e){
         type: 'POST',
         url: '/task/store',
         data: formData,
-        success: function(data){
+        success: function (data) {
             // reqest message clear
             $(msg).html('');
 
@@ -31,9 +31,9 @@ $('#createTaskForm').submit(function(e){
 
             // append result
             $('#taskTableBody').prepend(`
-                <tr data-id="`+data.id+`">
-                    <td> `+ data.id + ` </td>
-                    <td> `+ data.name + ` </td>
+                <tr data-id="` + data.id + `">
+                    <td> ` + data.id + ` </td>
+                    <td> ` + data.name + ` </td>
                     <td style="width:150px">
                         <a href="#" data-toggle="modal" data-target="#editTask" class="btn btn-sm btn-primary edit">Edit</a>
                         <a href="#" data-toggle="modal" data-target="#deleteTask"  class="btn btn-sm btn-danger delete">Delete</a>
@@ -41,15 +41,15 @@ $('#createTaskForm').submit(function(e){
                 </tr>
             `);
         },
-        error: function(error){
+        error: function (error) {
             $(msg).html('');
 
             $(msg).append('<ul id="errorMessage" class="alert alert-danger"></ul>')
 
-            $.each(error.responseJSON.errors, function(index, value){
+            $.each(error.responseJSON.errors, function (index, value) {
                 console.log(value[0]);
                 $(msg).find('#errorMessage').append(`
-                    <li>`+ value[0] +` </li>
+                    <li>` + value[0] + ` </li>
                 `);
             });
         }
@@ -57,28 +57,29 @@ $('#createTaskForm').submit(function(e){
 });
 
 // Edit Task
-$(document).on('click', '.edit', function(){
+$(document).on('click', '.edit', function () {
     let task = $(this).closest('tr').data('id');
     let modal = $('#editTaskForm');
 
     $.ajax({
         type: 'GET',
-        url: 'task/edit/'+task,
-        success: function(data){
+        url: 'task/edit/' + task,
+        success: function (data) {
             $(modal).find('#editInput').val(data.name);
             $(modal).attr('data-id', data.id);
         },
-        error: function(error){
+        error: function (error) {
             console.log(error);
         }
     });
+});
 
-    $('#editTaskForm button[type="submit"]').click({id: task}, call_ajax);
+// Update Task
+$('#editTaskForm').submit(function (e) {
+    e.preventDefault();
 
-    // Ajax call using function
-    function call_ajax(event){
     let msg = $('#editTaskMessage');
-    let id = event.data.id;
+    let id = $('#editTaskForm').data('id');
     // Form data
     let input = $('#editTaskForm #editInput');
     let formData  = {
@@ -121,76 +122,27 @@ $(document).on('click', '.edit', function(){
             });
         }
     })
-    }
-});
-
-// Update Task
-$('#editTaskForm').submit(function(e){
-    e.preventDefault();
-
-    // let msg = $('#editTaskMessage');
-    // let id = $('#editTaskForm').data('id');
-    // // Form data
-    // let input = $('#editTaskForm #editInput');
-    // let formData  = {
-    //     name: $(input).val()
-    // }
-
-    // console.log(id);
-    // console.log($('#editTaskForm').data('id'));
-
-    // $.ajax({
-    //     type: 'POST',
-    //     url: '/task/update/'+ id,
-    //     data: formData,
-    //     success: function(data){
-    //         // reqest message clear
-    //         $(msg).html('');
-
-    //         // Show success message
-    //         $(msg).append('<div class="alert alert-success"> Task updated successfully </div>');
-
-    //         // input value clear
-    //         $(input).val('');
-
-    //         // append result
-
-
-    //         let taskRow = $('#taskTableBody').find('tr[data-id="'+id+'"]');
-    //         $(taskRow).find('td.task-name').text(data.name);
-    //     },
-    //     error: function(error){
-    //         $(msg).html('');
-
-    //         $(msg).append('<ul id="errorMessage" class="alert alert-danger"></ul>')
-
-    //         $.each(error.responseJSON.errors, function(index, value){
-    //             console.log(value[0]);
-    //             $(msg).find('#errorMessage').append(`
-    //                 <li>`+ value[0] +` </li>
-    //             `);
-    //         });
-    //     }
-    // })
 });
 
 
 // Delete Popup
-$(document).on('click', '.delete', function(){
+$(document).on('click', '.delete', function () {
     let task = $(this).closest('tr').data('id');
     let modal = $('#deleteTaskForm');
 
     // Delete Confirmation
-    $('#deleteTaskForm button[type="submit"]').click({id: task}, call_ajax);
+    $('#deleteTaskForm button[type="submit"]').click({
+        id: task
+    }, call_ajax);
 
     // Ajax call using function
-    function call_ajax(event){
+    function call_ajax(event) {
         let msg = $('#deleteTaskMessage');
         let id = event.data.id;
         $.ajax({
             type: 'POST',
-            url: '/task/delete/'+ id,
-            success: function(data){
+            url: '/task/delete/' + id,
+            success: function (data) {
                 // reqest message clear
                 $(msg).html('');
 
@@ -200,11 +152,11 @@ $(document).on('click', '.delete', function(){
                 // Show success message
                 $(msg).append('<div class="alert alert-success"> Task deleted successfully </div>');
 
-                let taskRow = $('#taskTableBody').find('tr[data-id="'+id+'"]');
+                let taskRow = $('#taskTableBody').find('tr[data-id="' + id + '"]');
                 $(taskRow).remove();
                 console.log('task deleted');
             },
-            error: function(error){
+            error: function (error) {
 
             }
         })
@@ -212,7 +164,7 @@ $(document).on('click', '.delete', function(){
 });
 
 
-$('#deleteTaskForm').submit(function(e){
+$('#deleteTaskForm').submit(function (e) {
     e.preventDefault();
 });
 
@@ -239,5 +191,3 @@ $('#deleteTask').on('hidden.bs.modal', function (e) {
         <button type="submit" class="btn btn-danger">Yes, Delete</button>
     `);
 })
-
-
